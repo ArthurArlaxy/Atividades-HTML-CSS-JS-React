a066996abd9be261986441206b2c1c94af48cc05
Arthur Albuquerque <arthuraadeoliveira@gmail.com>
2025-04-21 09:25:20 -0300

    Atualizações

--- Fundamentos-CSS/Modulo-Bootstrap/projeto-1/src/js/index.js
+++ Fundamentos-CSS/Modulo-Bootstrap/projeto-1/src/js/index.js
@@ -3,3 +3,19 @@ import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+
+// Define o tema inicial baseado na preferência do usuário
+const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
+const htmlScheme = document.documentElement;
+
+if (prefersDarkScheme.matches) {
+    htmlScheme.setAttribute('data-bs-theme', 'dark');
+} else {
+    htmlScheme.setAttribute('data-bs-theme', 'light');
+}
+
+// Se o usuário mudar o tema do sistema enquanto a página está aberta
+prefersDarkScheme.addEventListener('change', e => {
+    htmlScheme.setAttribute('data-bs-theme', e.matches ? 'dark' : 'light');
+});
