fd745bf2a16389c76e5c1fb41390580966f06baf
Arthur Albuquerque <arthuraadeoliveira@gmail.com>
2025-04-24 11:14:43 -0300

    Atualizações

--- Fundamentos-CSS/Modulo-Bootstrap/Projeto-rede-social/src/js/main.js
+++ Fundamentos-CSS/Modulo-Bootstrap/Projeto-rede-social/src/js/main.js
@@ -1,5 +1,30 @@
-// Import our custom CSS
+// Importação de estilos e Bootstrap
 import '../scss/styles.scss'
-
-// Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+// Modal (opcional)
+const myModal = document.getElementById('post')
+const myInput = document.getElementById('post-input')
+
+if (myModal && myInput) {
+    myModal.addEventListener('shown.bs.modal', () => {
+        myInput.focus()
+    })
+}
+
+
+function automaticTheme() {
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    document.documentElement.setAttribute('data-bs-theme', prefersDark ? 'dark' : 'light');
+}
+
+automaticTheme()
+
+
+function switchTheme() {
+    const currentTheme = document.documentElement.getAttribute('data-bs-theme');
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    document.documentElement.setAttribute('data-bs-theme', newTheme);
+}
+
+document.getElementById("btn-theme").addEventListener('click',switchTheme)
