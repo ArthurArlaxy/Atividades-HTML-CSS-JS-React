2fa688e982ccfa53a9541e3276c718221602a0bb
Arthur Albuquerque <arthuraadeoliveira@gmail.com>
2025-04-25 20:54:53 -0300

    Updates

--- Fundamentos-CSS/Modulo-Bootstrap/Projeto-rede-social/src/js/main.js
+++ Fundamentos-CSS/Modulo-Bootstrap/Projeto-rede-social/src/js/main.js
@@ -1,8 +1,8 @@
 // Importação de estilos e Bootstrap
 import '../scss/styles.scss'
-import * as bootstrap from 'bootstrap'
+import { Dropdown , Modal, Carousel  } from 'bootstrap';
 
-// Modal (opcional)
+
 const myModal = document.getElementById('post')
 const myInput = document.getElementById('post-input')
 
@@ -12,19 +12,33 @@ if (myModal && myInput) {
     })
 }
 
-
 function automaticTheme() {
     const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
     document.documentElement.setAttribute('data-bs-theme', prefersDark ? 'dark' : 'light');
+    if(prefersDark !== true){
+        document.getElementById('btn-theme').classList.remove('bi-moon-stars')
+        document.getElementById('btn-theme').classList.add('bi-brightness-high')
+    }else{
+        document.getElementById('btn-theme').classList.add('bi-moon-stars')
+        document.getElementById('btn-theme').classList.remove('bi-brightness-high')
+    }
 }
 
-automaticTheme()
-
+document.addEventListener('DOMContentLoaded',automaticTheme)
 
 function switchTheme() {
     const currentTheme = document.documentElement.getAttribute('data-bs-theme');
     const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
     document.documentElement.setAttribute('data-bs-theme', newTheme);
+    if(currentTheme === 'dark'){
+        document.getElementById('btn-theme').classList.remove('bi-moon-stars')
+        document.getElementById('btn-theme').classList.add('bi-brightness-high')
+    }else{
+        document.getElementById('btn-theme').classList.add('bi-moon-stars')
+        document.getElementById('btn-theme').classList.remove('bi-brightness-high')
+    }
 }
 
 document.getElementById("btn-theme").addEventListener('click',switchTheme)
+
+//Funcionalidades da Rede Social
