0c3c4302616657790faba26c14d6eaac120ab877
Arthur Albuquerque <arthuraadeoliveira@gmail.com>
2025-07-11 07:04:49 -0300

    Atividades

--- Backend-JS-TS/lista-de-tarefa/src/model/listModel.js
+++ Backend-JS-TS/lista-de-tarefa/src/model/listModel.js
@@ -4,12 +4,20 @@ const fs = require('node:fs')
 
 const listModel = {
     getAllLists(){
-        let lists = fs.readdirSync(path.join(__dirname, '..', 'db')) 
+        const lists = fs.readdirSync(path.join(__dirname, '..', 'db')) 
         return lists
     },
-    createList(){
-        return ''
-    }
+    createList(title){
+        const dirPath = fs.mkdirSync(path.join(__dirname,'..','db',title),)
+
+        if(fs.exisysSync(dirPath)){
+            return alert('Lista com esse nome já existe!')
+        } 
+
+        fs.mkdirSync(dirPath,{ recursive:true })
+        alert('Lista criada com sucesso!')
+    },
+    
 }
 
 module.exports = listModel
