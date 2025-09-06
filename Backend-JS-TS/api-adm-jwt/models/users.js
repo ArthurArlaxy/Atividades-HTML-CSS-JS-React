28d1a00256c3364fae97f5f7cc78487961401fa2
Arthur Albuquerque <arthuraadeoliveira@gmail.com>
2025-09-06 09:06:53 -0300

    Atividades

--- Backend-JS-TS/api-adm-jwt/models/users.js
+++ Backend-JS-TS/api-adm-jwt/models/users.js
@@ -1,5 +1,6 @@
 const users = [
-    { username:'Arlaxy', email:'arlaxy@gmail.com', password:'123', role:"Administrator"}
+    { username:'Arlaxy', email:'arlaxy@gmail.com', password:'123',role:"Administrator"},
+    { username:'Rafael', email:'rafael@gmail.com', password:'123', role:"Administrator"}
 ]
 
 module.exports = users
