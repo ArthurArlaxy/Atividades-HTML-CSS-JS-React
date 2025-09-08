746f2f5c8554078cbfd31d9ffba6550a45d3edc8
Arthur Albuquerque <arthuraadeoliveira@gmail.com>
2025-09-08 16:57:07 -0300

    Atividades

--- Fundamentos-JS/validacao_de_formulario/app.js
+++ Fundamentos-JS/validacao_de_formulario/app.js
@@ -1,7 +1,7 @@
 function validEmail(email){
-    emailValidation = email.match(/\w{2,}@[a-zA-Z]{2,}\.[a-zA-Z]{2,}/g)[0]
+    emailValidation = email.match(/\w{2,}@[a-zA-Z]{2,}\.[a-zA-Z]{2,}/g)
     if (!emailValidation){
-        const err =  Error('O email é invalido')
+        const err =  Error('O email é invalido, precisa seguir o modelo "__@__.__"')
         err.input = 'email'
         throw err
     }
@@ -12,8 +12,8 @@ function validPassword(password) {
     const passwordValidation3 = password.match(/[0-9]/);
     const passwordValidation4 = password.match(/[^A-Za-z0-9]/);
 
-    if (!passwordValidation || !passwordValidation2 || !passwordValidation3 || !passwordValidation4 || password < 8) {
-        const err = Error('A senha é inválida');
+    if (!passwordValidation || !passwordValidation2 || !passwordValidation3 || !passwordValidation4 || password.length < 8) {
+        const err = Error("A senha é inválida. Ela deve conter: pelo menos 1 letra maiúscula, 1 letra minúscula, 1 número e 1 caractere especial, além de ter no mínimo 8 caracteres.");
         err.input = 'password';
         throw err;
     }
