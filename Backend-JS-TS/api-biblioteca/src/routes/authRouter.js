6da7f52adfa6896245c02b49c82fea2aaca0c072
Arthur Albuquerque <arthuraadeoliveira@gmail.com>
2025-09-24 20:25:08 -0300

    remove API-biblioteca

--- Backend-JS-TS/api-biblioteca/src/routes/authRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const usersController = require('../controller/usersController')
-const authRouter = express.Router()
-
-
-authRouter.post('/register', usersController.register)
-authRouter.post('/login', usersController.login)
-
-module.exports = authRouter
