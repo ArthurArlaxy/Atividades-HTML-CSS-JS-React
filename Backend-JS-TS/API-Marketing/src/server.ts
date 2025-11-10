a557e7273992d3d7949af2b77fc5de355f85c874
Arthur Albuquerque <arthuraadeoliveira@gmail.com>
2025-11-10 19:35:20 -0300

    Create server.ts

---
+++ Backend-JS-TS/API-Marketing/src/server.ts
@@ -0,0 +1,15 @@
+import cors from "cors"
+import express from "express"
+import { router } from "./router.js"
+import { errorHandlerMiddleware } from "./middlewares/errorHandler.js"
+
+const app = express()
+
+app.use(cors())
+app.use("/api", router)
+app.use(errorHandlerMiddleware)
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Server iniciado em http://localhost:3000`)
+})
