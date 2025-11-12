ede4091969e26cae2b2bf2237d8130d0d4eba375
Arthur Albuquerque <arthuraadeoliveira@gmail.com>
2025-11-11 21:04:09 -0300

    Update router.ts

--- Backend-JS-TS/API-Marketing/src/router.ts
+++ Backend-JS-TS/API-Marketing/src/router.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { HttpError } from "./errors/HttpError.js";
+import { LeadsControllers } from "./controllers/LeadsControllers.js";
 
 export const router = Router()
 
-router.get("/", async (req, res, next) => {
-    res.json({message: "ok"})
-})
+router.get("/leads", LeadsControllers.index)
+router.post("/leads", LeadsControllers.create)
+router.get("/leads/:id", LeadsControllers.show)
+router.put("/leads/:id", LeadsControllers.update)
+router.delete("/leads/:id", LeadsControllers.delete)
