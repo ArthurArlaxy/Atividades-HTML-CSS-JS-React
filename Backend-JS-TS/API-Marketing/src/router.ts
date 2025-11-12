a9800d606a558d18cda601ca6b3769973783a3cb
Arthur Albuquerque <arthuraadeoliveira@gmail.com>
2025-11-12 19:01:29 -0300

    Update router.ts

--- Backend-JS-TS/API-Marketing/src/router.ts
+++ Backend-JS-TS/API-Marketing/src/router.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { LeadsControllers } from "./controllers/LeadsControllers.js";
+import { GroupsController } from "./controllers/GroupsControllers.js";
+import { CampaignsController } from "./controllers/CampaignControllers.js";
 
 export const router = Router()
 
@@ -8,3 +10,15 @@ router.post("/leads", LeadsControllers.create)
 router.get("/leads/:id", LeadsControllers.show)
 router.put("/leads/:id", LeadsControllers.update)
 router.delete("/leads/:id", LeadsControllers.delete)
+
+router.get("/groups", GroupsController.index)
+router.post("/groups", GroupsController.create)
+router.get("/groups/:id", GroupsController.show)
+router.put("/groups/:id", GroupsController.update)
+router.delete("/groups/:id", GroupsController.delete)
+
+router.get("/campaigns", CampaignsController.index)
+router.post("/campaigns", CampaignsController.create)
+router.get("/campaigns/:id", CampaignsController.show)
+router.put("/campaigns/:id", CampaignsController.update)
+router.delete("/campaigns/:id", CampaignsController.delete)
