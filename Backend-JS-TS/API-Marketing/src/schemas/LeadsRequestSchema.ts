a2ddd98117f5ed4de49dacb861d7dd72ebdeeffb
Arthur Albuquerque <arthuraadeoliveira@gmail.com>
2025-11-13 07:18:43 -0300

    Update LeadsRequestSchema.ts

--- Backend-JS-TS/API-Marketing/src/schemas/LeadsRequestSchema.ts
+++ Backend-JS-TS/API-Marketing/src/schemas/LeadsRequestSchema.ts
@@ -1,15 +1,27 @@
 import { z } from "zod"
 
+
+const StatusEnumSchema = z.enum([  "New","Contacted","Qualified","Converted","Unresponsive","Disqualified","Archived"])
+
+export const GetLeadsSchema = z.object({
+    page: z.string().optional(),
+    pageSize: z.string().optional(),
+    name: z.string().optional(),
+    status: StatusEnumSchema.optional(),
+    sortBy: z.enum(["name", "status", "createdAt", "updatedAt"]).optional(),
+    order: z.enum(["asc", "desc"]).optional()
+})
+
 export const CreateLeadRequestSchema = z.object({
     name: z.string(),
     email: z.string(),
     phone: z.string(),
-    status: z.enum([  "New","Contacted","Qualified","Converted","Unresponsive","Disqualified","Archived"]).optional()
+    status: StatusEnumSchema.optional()
 })
 
 export const UpdateLeadRequestSchema = z.object({
     name: z.string().optional(),
     email: z.string().optional(),
     phone: z.string().optional(),
-    status: z.enum([  "New","Contacted","Qualified","Converted","Unresponsive","Disqualified","Archived"]).optional()
+    status: StatusEnumSchema.optional()
 })
